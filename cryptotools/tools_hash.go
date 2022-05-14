09637031af5080e53449536b5232ce96ddf1e473
KBlackMeow <wendell.kami@outlook.com>
2022-05-14 20:06:26 +0800

    *

---
+++ cryptotools/tools_hash.go
@@ -0,0 +1,30 @@
+package cryptotools
+
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
+
+var Salt = fmt.Sprint(time.Now().UnixMilli() % 556639)
+
+func Hash_SHA512(str string) string {
+	str = str + Salt
+	hashbyt := sha512.Sum512([]byte(str))
+	return hex.EncodeToString(hashbyt[:])
+}
+
+func Hash_SHA256(str string) string {
+	str = str + Salt
+	hashbyt := sha256.Sum256([]byte(str))
+	return hex.EncodeToString(hashbyt[:])
+}
+
+func Hash_MD5(str string) string {
+	str = str + Salt
+	hashbyt := md5.Sum([]byte(str))
+	return hex.EncodeToString(hashbyt[:])
+}
