28ac5881040d827936755db4139fdc2edcc8447b
KBlackMeow <wendell.kami@outlook.com>
2022-05-16 20:41:02 +0800

    *

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"shadowproxy/config"
 	"shadowproxy/proxy"
@@ -17,13 +16,6 @@ func main() {
 
 	help := flag.Bool("help", false, "print usage")
 	cfg := flag.String("config", "", "use config file")
-	bind := flag.String("bind", "127.0.0.1:30000", "The address to bind to")
-	backend := flag.String("backend", "", "The backend server address")
-	protocol := flag.String("protocol", "tcp", "To use tcp, udp or tcp/udp")
-	shadow := flag.String("shadow", "", "The shadow server address")
-	loglevel := flag.Int("loglevel", 0, "The level of log 0:all, 1 warn and error,2 error")
-	password := flag.String("password", "admin", "password of auth system,and it must not be empty")
-	enableFillter := flag.Bool("fillter", true, "enable fillter")
 
 	flag.Parse()
 
@@ -36,13 +28,7 @@ func main() {
 
 		config.FilePath = *cfg
 		config.GetConfig()
-		config.ShadowProxyConfig.BindAddr = *bind
-		config.ShadowProxyConfig.BackendAddr = *backend
-		config.ShadowProxyConfig.Protocol = *protocol
-		config.ShadowProxyConfig.Shadow = *shadow
-		config.ShadowProxyConfig.LogLevel = fmt.Sprint(*loglevel)
-		config.ShadowProxyConfig.Password = *password
-		config.ShadowProxyConfig.EnableFillter = *enableFillter
+
 	}
 
 	config.InitComponentConfig()
