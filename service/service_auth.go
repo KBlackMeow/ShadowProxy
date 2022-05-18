5e59fe90f5eb56798e9174d6468e468584b97cc0
KBlackMeow <wendell.kami@outlook.com>
2022-05-18 23:41:47 +0800

    *

--- service/service_auth.go
+++ service/service_auth.go
@@ -79,7 +79,6 @@ func (service AuthService) verify(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if (time.Now().UnixMilli()-msgUnixTime) > 0 && (time.Now().UnixMilli()-msgUnixTime) < 1000 && password == cryptotools.Hash_SHA512(config.ShadowProxyConfig.Password) {
-
 			fillter.AppendWhiteList(remoteAddr)
 
 			userinfo := UserInfo{UserAddr: remoteAddr, UserLoginTime: logger.TimeNow()}
