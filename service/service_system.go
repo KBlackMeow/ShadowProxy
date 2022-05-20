7494a9459c5924a2369b05b6bf2a68d71d25f9c6
KBlackMeow <wendell.kami@outlook.com>
2022-05-20 22:20:15 +0800

    *

---
+++ service/service_system.go
@@ -0,0 +1,44 @@
+package service
+
+import (
+	"os/exec"
+	"shadowproxy/config"
+	"shadowproxy/logger"
+	"strings"
+)
+
+type SystemService struct {
+	Service
+}
+
+func (service SystemService) Run() {
+
+	for _, v := range config.ShadowProxyConfig.CMD {
+		args := strings.Split(v, " ")
+		command := exec.Command(args[0], args[1:]...)
+		err := command.Start()
+		if err != nil {
+			logger.Error("cmd", err)
+		}
+	}
+
+}
+
+func (service SystemService) GetAddr() string {
+
+	return service.serviceAddr
+
+}
+
+func (service SystemService) GetName() string {
+
+	return service.serviceName
+
+}
+
+func init() {
+
+	service := SystemService{Service{serviceName: "cmd", serviceAddr: "999.999.999.999:9999"}}
+	ServiceAppend("cmd", service)
+
+}
