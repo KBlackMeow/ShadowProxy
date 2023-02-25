3272c546c541558ef2d8a81a0f664cc841339181
KBlackMeow <wendell.kami@outlook.com>
2023-02-25 23:53:51 +0800

    *

--- service/service_auth1.go
+++ service/service_auth1.go
@@ -58,48 +58,47 @@ func (service AuthService1) auth() {
 			Message := AuthMessage{Token: service.token(addr.String()), Pubkey: cryptotools.GetKey("public.pem")}
 			data, _ := json.Marshal(&Message)
 			go service.listener.WriteToUDP(data, addr)
-
-		} else {
-
-			cryptedMessage := loginMessage.Message
-			Message := cryptotools.DecryptRSAToString(cryptedMessage)
-			Messages := strings.Split(Message, "#")
-			remoteAddr := addr.String()
-
-			if Message == "" || len(Messages) != 3 {
-				logger.Warn("Auth1", remoteAddr, "RSA Public Key is wrong")
-				time.Sleep(time.Duration(3000) * time.Millisecond)
-				continue
-			}
-
-			password := cryptotools.Hash_SHA512(Messages[0])
-			MessageUnixTime, _ := strconv.ParseInt(Messages[1], 10, 64)
-			MessageUnixTime = int64(MessageUnixTime)
-
-			token := Messages[2]
-			if !service.verifyToken(remoteAddr, token) {
-				logger.Warn("Auth1", remoteAddr, "Token is wrong")
-				time.Sleep(time.Duration(3000) * time.Millisecond)
-				continue
-			}
-
-			if (time.Now().UnixMilli()-MessageUnixTime) > 0 && (time.Now().UnixMilli()-MessageUnixTime) < 1000 &&
-				password == cryptotools.Hash_SHA512(config.ShadowProxyConfig.Password) {
-				filter.AppendWhiteList(remoteAddr, 10000)
-				continue
-			}
-
-			if password != cryptotools.Hash_SHA512(config.ShadowProxyConfig.Password) {
-				logger.Warn("Auth1", remoteAddr, "Password is wrong")
-			} else if (time.Now().UnixMilli() - MessageUnixTime) > 1000 {
-				logger.Warn("Auth1", remoteAddr, "Unix Time exceed the time limit")
-			} else {
-				logger.Warn("Auth1", remoteAddr, "Alice is attacking the server")
-			}
-
-			time.Sleep(time.Duration(3000) * time.Millisecond)
+			continue
 
 		}
+
+		cryptedMessage := loginMessage.Message
+		Message := cryptotools.DecryptRSAToString(cryptedMessage)
+		Messages := strings.Split(Message, "#")
+		remoteAddr := addr.String()
+
+		if Message == "" || len(Messages) != 3 {
+			logger.Warn("Auth1", remoteAddr, "RSA Public Key is wrong")
+			time.Sleep(time.Duration(3000) * time.Millisecond)
+			continue
+		}
+
+		password := cryptotools.Hash_SHA512(Messages[0])
+		MessageUnixTime, _ := strconv.ParseInt(Messages[1], 10, 64)
+		MessageUnixTime = int64(MessageUnixTime)
+
+		token := Messages[2]
+		if !service.verifyToken(remoteAddr, token) {
+			logger.Warn("Auth1", remoteAddr, "Token is wrong")
+			time.Sleep(time.Duration(3000) * time.Millisecond)
+			continue
+		}
+
+		if (time.Now().UnixMilli()-MessageUnixTime) > 0 && (time.Now().UnixMilli()-MessageUnixTime) < 1000 &&
+			password == cryptotools.Hash_SHA512(config.ShadowProxyConfig.Password) {
+			filter.AppendWhiteList(remoteAddr, 10000)
+			continue
+		}
+
+		if password != cryptotools.Hash_SHA512(config.ShadowProxyConfig.Password) {
+			logger.Warn("Auth1", remoteAddr, "Password is wrong")
+		} else if (time.Now().UnixMilli() - MessageUnixTime) > 1000 {
+			logger.Warn("Auth1", remoteAddr, "Unix Time exceed the time limit")
+		} else {
+			logger.Warn("Auth1", remoteAddr, "Alice is attacking the server")
+		}
+
+		time.Sleep(time.Duration(3000) * time.Millisecond)
 
 	}
 
