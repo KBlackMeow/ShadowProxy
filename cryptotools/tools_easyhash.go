97c3fe6ae57967e49ea01478169e4e1e5dd15710
KBlackMeow <wendell.kami@outlook.com>
2023-03-11 18:29:14 +0800

    *

--- cryptotools/tools_easyhash.go
+++ cryptotools/tools_easyhash.go
@@ -3,7 +3,7 @@ package cryptotools
 func EasyHash_uint64(data string) uint64 {
 	code := uint64(0)
 	for i, v := range data {
-		code += uint64(i) * uint64(v)
+		code += uint64(i) * uint64(v) % 65537
 	}
 	return code
 }
